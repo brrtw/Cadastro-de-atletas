57420bc6ee88f0a007f2c1a2641d30f40f9bcef5
brrtw <114107361+brrtw@users.noreply.github.com>
2025-07-28 15:28:49 -0300

    atletas.c

--- atletas.c
+++ atletas.c
@@ -1,34 +1,3 @@
-#ifndef ATLETAS_H
-#define ATLETAS_H
-
-typedef struct {
-    char nome[50];
-    char posicao[50];
-    int idade;
-} Atleta;
-
-typedef struct No {
-    Atleta atleta;
-    struct No* prox;
-} No;
-
-
-typedef int bool;
-#define true 1
-#define false 0
-
-No* carregarAtletas();
-void salvarAtletas(No* lista);
-void inserirAtleta(No** lista, Atleta novo);
-void listarAtletas(No* lista);
-void ordenarPorNome(No** lista);
-void buscarBinariaNome(No* lista, const char* nome);
-void editarAtleta(No* lista, const char* nomeBusca);
-void deletarAtleta(No** lista, const char* nomeBusca);
-void liberarLista(No* lista);
-
-#endif
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
